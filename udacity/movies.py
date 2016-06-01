f465533f1a63ce5046bae755601dc9124862e081
Mita Mercury <omgninjas@me.com>
2016-06-01 16:37:42 +0000

    Add recent lessons

---
+++ udacity/movies.py
@@ -0,0 +1,8 @@
+#title
+#storyline
+
+def show_trailer(title, url):
+    # Open browser and play trailer
+
+def show_info():
+    # Print movie information - title, storyline, release_date, rating, url, director, box_office
