11378cd08ef0e082c6829680dfc02a3ca5fda1a8
Damita Salmon <DSalmon@PENNONI.COM>
2016-06-27 13:44:23 -0400

    Update file tree

--- udacity/mindstorms.py
+++ udacity/mindstorms.py
@@ -1,13 +1,26 @@
 import turtle
 
-def draw_square():
+def draw_square(some_turtle):
+	for i in range(1, 5):
+		some_turtle.forward(100)
+		some_turtle.right(90)
+
+def draw_art():
     window = turtle.Screen()
     window.bgcolor('red')
     
     brad = turtle.Turtle()
-    brad.forward(100)
-    brad.right(90)
+    brad.shape('turtle')
+    brad.color('yellow')
+    brad.speed(2)
+	draw_square(brad)
+    
+    angie = turtle.Turtle()
+    angie.shape('arrow')
+    angie.color('blue')
+    angie.circle(100)
+    angie.speed(2)
     
     window.exitonclick()
-    
-draw_square()
+
+draw_art()
